0426d20fe1227834d4f43ee9f457debb4a7b19a1
Ethan Swearingen <eswearingen2@student.cscc.edu>
2026-02-15 16:26:03 -0500

    Printing Dice Totals

--- main.py
+++ main.py
@@ -11,3 +11,6 @@ while True:
     dice1 = random.randint(1,6) # Will roll two ints 1-6 every pass throught the while loop
     dice2 = random.randint(1,6)
 
+    print(f"Dice 1: {dice1}") # Prints the individual and combined totals of the dice. Will repeat with every loop.
+    print(f"Dice 2: {dice2}")
+    print(f"Dice total: {dice1 + dice2}")
