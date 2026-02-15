3272990b40462deaf5153f6494670d3255de373a
Ethan Swearingen <eswearingen2@student.cscc.edu>
2026-02-15 16:23:14 -0500

    Initializing Dice Rolls.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Dice Rolling Terms,
+Ethan Swearingen,
+Rolls two dice and prints a corresponding term,
+Starter code is mine,
+02/15/2026
+"""
+import random
+
+while True:
+    dice1 = random.randint(1,6) # Will roll two ints 1-6 every pass throught the while loop
+    dice2 = random.randint(1,6)
+
